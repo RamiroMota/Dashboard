07dd5edca7e0ade0f0a134709b3c323ae347f8de
RamiroMota <rockett117@gmail.com>
2024-11-08 12:50:40 -0600

    Se actualizo el almacenamiento

--- src/components/pages/doc_almace/ProgressBar.tsx
+++ src/components/pages/doc_almace/ProgressBar.tsx
@@ -8,7 +8,7 @@ export const ProgressBar: React.FC<ProgressBarProps> = ({ progress }) => {
   return (
     <div className="w-full bg-gray-200 rounded-full h-2.5 overflow-hidden">
       <div
-        className="bg-blue-500 h-full transition-all duration-300 ease-out"
+        className="bg-orange-500 h-full transition-all duration-300 ease-out"
         style={{ width: `${progress}%` }}
       />
     </div>
